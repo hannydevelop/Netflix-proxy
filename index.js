46d58adcb5a6e19c53de561bfa13c17847fa5a67
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-06-05 21:32:37 +0100

    added access control to request

--- index.js
+++ index.js
@@ -3,32 +3,20 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 // Creating express server
 const app = express();
 
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 
 // Proxy Logic :  Proxy endpoints
 app.use(
     "/netlify",
     createProxyMiddleware({
         target: 'https://api.netlify.com/api/v1/sites',
+        on: {
+            proxyReq: (proxyReq, req, res) => {
+              /* handle proxyReq */
+            },
+            proxyRes: (proxyRes, req, res) => {
+                proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+            },
+        },
         changeOrigin: true,
         pathRewrite: {
             "^/netlify": "",
